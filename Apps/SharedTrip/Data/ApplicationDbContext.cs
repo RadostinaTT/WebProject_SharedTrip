fea13df48554659fcfc60784e54fe19c8f0236da
RadostinaTT <radtod77@icoud.com>
2021-02-01 21:33:59 +0200

    Add user controller and services

--- Apps/SharedTrip/Data/ApplicationDbContext.cs
+++ Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -12,5 +12,24 @@
             : base(db)
         {
         }
+
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserTrip> UserTrips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) 
+            {
+                optionsBuilder.UseSqlServer("Server=LAPTOP-0TN9PVA8;Database=SharedTrip;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<UserTrip>().HasKey(x => new { x.UserId, x.TripId });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
